31135864cf255474aed3082aa656e2df0722f7b2
teten <tennugraha777@gmail.com>
2018-12-10 10:48:20 +0700

    update

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
 
 class Post(models.Model) :
+
+    objects = models.Manager 
+    published  = PublishedManager()
+
     STATUS_CHOICES = (
         ('draft','Draft'),
         ('published','Published'),
@@ -22,7 +31,14 @@ class Post(models.Model) :
                                      default='draft')
 
     class Meta:
-        ordering = ('-publish')
+        ordering = ('-publish',)
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.strftime('%m'),
+                             self.publish.strftime('%d'),
+                             self.slug])
 
     def __str__(self):
         return self.title
